e06d04555f2ca6635b41f48eab84782b75d04d91
Ilya Yachin <uin153974748@gmail.com>
2021-06-08 13:07:46 +1000

    examples excluded from package destribution

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftCoAP",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["Example_Projects"]),
         .testTarget(
             name: "SwiftCoAPTests",
             dependencies: ["SwiftCoAP"]),
